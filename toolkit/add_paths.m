ed1b2d3ec9e49e1727d9094262f917ccfed09b76
Ivan Antešić <ia6382@student.uni-lj.si>
2023-02-17 18:38:55 +0100

    Delete toolkit directory

--- toolkit/add_paths.m
+++
@@ -1,9 +0,0 @@
-function toolkit_directory = add_paths()
-
-toolkit_directory = fileparts(mfilename('fullpath'));
-include_dirs = cellfun(@(x) fullfile(toolkit_directory, x), ...
-    {'', 'utils', 'examples'}, 'UniformOutput', false);
-
-addpath(include_dirs{:});
-
-end  % endfunction
--- toolkit/create_tracker_workspace.m
+++
@@ -1,7 +0,0 @@
-function create_tracker_workspace()
-
-p = add_paths();
-
-copyfile(fullfile(p, 'workspace_config_template.m'), './workspace_config.m');
-
-end  % endfunction
